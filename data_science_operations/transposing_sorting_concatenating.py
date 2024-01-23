d1a97f168eb5ac6b2f25fdf2d2e526332c0d5ce9
dswalla <derek.scott.wallace@gmail.com>
2024-01-22 23:19:23 -0700

    Transpose tutorial

---
+++ data_science_operations/transposing_sorting_concatenating.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+print("Transposing")
+print("-----------------")
+
+a = np.array([
+    [1,2],
+    [3,4],
+    [5,6]
+])
+
+print(a.T)
+print("-----------------")
+
+print(a.transpose())
