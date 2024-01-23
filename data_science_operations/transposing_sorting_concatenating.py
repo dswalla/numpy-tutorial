dfebd231b32b074e00a14c650c0c388b1f9c3132
dswalla <derek.scott.wallace@gmail.com>
2024-01-22 23:24:05 -0700

    Sorting tutorial

--- data_science_operations/transposing_sorting_concatenating.py
+++ data_science_operations/transposing_sorting_concatenating.py
@@ -13,3 +13,22 @@ print(a.T)
 print("-----------------")
 
 print(a.transpose())
+print("-----------------")
+
+print("Sorting")
+print("-----------------")
+
+data = np.array([
+    [7,1,4],
+    [8,6,5],
+    [1,2,3]
+])
+
+print(np.sort(data))
+print("-----------------")
+
+print(np.sort(data, axis=None))
+print("-----------------")
+
+print(np.sort(data, axis=0))
+print("-----------------")
